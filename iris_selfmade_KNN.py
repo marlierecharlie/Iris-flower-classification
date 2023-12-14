cf72c738d69ee5a8593dd5c0e7d1fbe292449d06
charlie marliere <charlie.marliere@gmail.com>
2023-12-14 15:30:39 +0100

    test coherence betwen file and test file

--- iris_selfmade_KNN.py
+++ iris_selfmade_KNN.py
@@ -6,7 +6,7 @@ class KNN:
 	def fit(self, x_train, y_train):
 		self.x_train = x_train
 		self.y_train = y_train
-	
+
 	def predict(self, x_test):
 		predictions = []
 		for row in x_test:
@@ -29,7 +29,7 @@ iris = load_iris()
 x = iris.data
 y = iris.target
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
 clf = KNN()
