3f7d1a3fcf771a0fc47dc0bb356fb46995c87e95
charlie marliere <charlie.marliere@gmail.com>
2023-12-14 16:40:54 +0100

    add fastapi

--- iris_selfmade_KNN.py
+++ iris_selfmade_KNN.py
@@ -1,4 +1,7 @@
 from scipy.spatial import distance
+from fastapi import FastAPI, HTTPException
+from sklearn.metrics import accuracy_score as acs
+import uvicorn
 def eu(a, b):
 	return distance.euclidean(a, b)
 
@@ -34,7 +37,17 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
 clf = KNN()
 clf.fit(x_train, y_train)
-prediction = clf.predict(x_test)
 
-from sklearn.metrics import accuracy_score as acs
-print(acs(prediction, y_test))
+app = FastAPI()
+@app.post("/predict")
+async def pred(x_test):
+    try :
+        prediction = clf.predict(x_test)
+        result = (acs(prediction, y_test))
+        return {"prediction": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+# Run the API with uvicorn
+# Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run("iris_selfmade_KNN:app", host='127.0.0.1', port=8000, reload=True)
